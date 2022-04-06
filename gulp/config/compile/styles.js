0e3457452f3778fd83f9f9f0ebda127b6b2a38e6
borgmon <borgmon7@gmail.com>
2022-04-05 21:17:37 -0700

    fix build

--- gulp/config/compile/styles.js
+++ gulp/config/compile/styles.js
@@ -1,12 +1,10 @@
 const
   gulp = require('gulp'),
   cssImport = require('gulp-cssimport'),
-  $ = require('gulp-load-plugins')();
-
+  sass = require('gulp-sass')(require('sass'));
 gulp.task('styles', function () {
   return gulp.src('app/styles/*.scss')
-    .pipe($.plumber())
-    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(cssImport())
     .pipe(gulp.dest('public/styles'));
 });
